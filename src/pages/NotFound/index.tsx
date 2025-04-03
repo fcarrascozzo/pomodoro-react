c29c57661da2001baa151c8f37408f91aee46b6d
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-02 21:30:22 -0300

    Criando Template

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nulla
+          aliquid natus voluptate iste id tempore aliquam, esse quidem
+          praesentium eligendi veritatis sint. Aspernatur minus suscipit quas
+          consequatur voluptas at.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
