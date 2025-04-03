73a266b6527cf1d5161fd679e556ad42a46038d0
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-02 21:48:57 -0300

    Adicionando pagina notFound e pagina aboutPomodoro

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <a href=''>Entenda como funciona a técnica pomodoro</a>
-      <a href=''>Devodoro Pomodoro &copy; {new Date().getFullYear()}</a>
+      <a href=''>Chronos Pomodoro &copy; {new Date().getFullYear()}</a>
     </footer>
   );
 }
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,7 +6,7 @@ export function Logo() {
     <div className={styles.logo}>
       <a className={styles.logoLink} href='#'>
         <TimerIcon />
-        <span>Devodoro</span>
+        <span>Chronos</span>
       </a>
     </div>
   );
