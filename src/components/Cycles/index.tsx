eda26a0ef827874cb3bb5e4141c033be57d81c4e
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-05 17:02:14 -0300

    Exibindo cycles de forma dinamica

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,19 +1,37 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskState';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 import styles from './styles.module.css';
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycleStep = Array.from({ length: state.currentCycle });
+
+  const cycleDescription = {
+    workTime: 'foco',
+    shortBreakTime: 'descanso curto',
+    longBreakTime: 'descanso longo',
+  };
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos: </span>
 
       <div className={styles.cycleDots}>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
+        {cycleStep.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+
+          return (
+            <span
+              key={nextCycle}
+              className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+              area-label={`Indicador de ciclo de ${cycleDescription[nextCycleType]}`}
+              title={`Indicador de ciclo de ${cycleDescription[nextCycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
