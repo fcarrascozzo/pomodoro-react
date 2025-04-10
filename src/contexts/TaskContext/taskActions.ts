6e478ec37a1c78d976a8721ddeddccd51ebf6ba6
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-09 22:06:26 -0300

    Atualizando o contador a cada segundo no estado e na pagina

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -3,18 +3,25 @@ import { TaskModel } from "../../models/TaskModel";
 export enum TaskActionTypes {
     START_TASK = 'START_TASK',
     INTERRUPT_TASK = 'INTERRUPT_TASK',
-    RESET_STATE = 'RESET_STATE'
+    RESET_STATE = 'RESET_STATE',
+    COUNT_DOWN = 'COUNT_DOWN',
+    COMPLETE_TASK = 'COMPLETE_TASK',
 }
 
 export type TaskActionWithPayload = {
     type: TaskActionTypes.START_TASK;
     payload: TaskModel;
+} | {
+    type: TaskActionTypes.COUNT_DOWN;
+    payload: {secondsRemaining: number};
 };
 
 export type TaskActionWithoutPayload =  {
     type: TaskActionTypes.INTERRUPT_TASK;
 } | {
     type: TaskActionTypes.RESET_STATE;
+} | {
+    type: TaskActionTypes.COMPLETE_TASK;
 }
 
 export type TaskActionModel = TaskActionWithPayload | TaskActionWithoutPayload;
