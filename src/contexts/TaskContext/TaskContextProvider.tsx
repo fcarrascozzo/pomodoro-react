90d571e18ca5ea533b506142b26ee1630e07cf0c
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-14 22:07:18 -0300

    Atualizando o t´tiulo da página a cada segundo da tarefa

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,41 +1,40 @@
-import { useEffect, useReducer, useRef } from 'react';
-import { initialTaskState } from './initialTaskState';
-import { TaskContext } from './TaskContext';
-import { taskReducer } from './taskReducer';
-import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
-import { TaskActionTypes } from './taskActions';
-import { loadBeep } from '../../utils/loadBeep';
+import { useEffect, useReducer, useRef } from "react";
+import { initialTaskState } from "./initialTaskState";
+import { TaskContext } from "./TaskContext";
+import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
+import { TaskActionTypes } from "./taskActions";
+import { loadBeep } from "../../utils/loadBeep";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   const worker = TimerWorkerManager.getInstance();
 
-  worker.onmessage(e => {
+  worker.onmessage((e) => {
     const countDownSeconds = e.data;
-    
+
     if (countDownSeconds <= 0) {
-      if(playBeepRef.current) {
+      if (playBeepRef.current) {
         playBeepRef.current();
         playBeepRef.current = null;
       }
- 
+
       dispatch({
-        type: TaskActionTypes.COMPLETE_TASK
-      })
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
 
       worker.terminate();
     } else {
       dispatch({
         type: TaskActionTypes.COUNT_DOWN,
-        payload: {secondsRemaining: countDownSeconds}
-      })
+        payload: { secondsRemaining: countDownSeconds },
+      });
     }
   });
 
@@ -44,17 +43,19 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
       worker.terminate();
     }
 
+    document.title = `${state.formattedSecondsRemaining} - Chronos Pomodoro`;
+
     worker.postMessage(state);
   }, [worker, state]);
 
   useEffect(() => {
-    if(state.activeTask && playBeepRef.current === null) {
-      playBeepRef.current = loadBeep(); 
-    } else { 
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
       playBeepRef.current = null;
     }
   }, [state.activeTask]);
- 
+
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
       {children}
