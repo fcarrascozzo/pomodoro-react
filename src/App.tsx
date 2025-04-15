4b1c140d424ba9ed6c650dd74444f4e8499d5e0b
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-14 21:27:04 -0300

    Toastify com adapter, adicionando lib para mensagens reutilizaveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Home } from './pages/Home';
-import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
+import { Home } from "./pages/Home";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
+import { MessagesContainer } from "./components/MessagesContainer";
 
-import './styles/theme.css';
-import './styles/global.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
   return (
     <TaskContextProvider>
-      <Home />;
+      <MessagesContainer>
+        <Home />;
+      </MessagesContainer>
     </TaskContextProvider>
   );
 }
