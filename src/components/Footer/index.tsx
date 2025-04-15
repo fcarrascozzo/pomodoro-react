cbb86bc869dc1db7b55cea1996d3fc3ddbd698e4
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-14 22:03:22 -0300

    Criando Link unificado para o React Router

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router";
+import { RouterLink } from "../RouterLink";
 import styles from "./styles.module.css";
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link to="/about-pomodoro">Entenda como funciona a técnica pomodoro</Link>
-      <Link to="/">Chronos Pomodoro &copy; {new Date().getFullYear()}</Link>
+      <RouterLink href="/about-pomodoro">
+        Entenda como funciona a técnica pomodoro
+      </RouterLink>
+      <RouterLink href="/">
+        Chronos Pomodoro &copy; {new Date().getFullYear()}
+      </RouterLink>
     </footer>
   );
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,9 +6,9 @@ import {
   SunIcon,
 } from "lucide-react";
 import { useState, useEffect } from "react";
+import { RouterLink } from "../RouterLink";
 
 import styles from "./styles.module.css";
-import { Link } from "react-router";
 
 type AvailableThemes = "dark" | "light";
 
@@ -44,30 +44,30 @@ export function Menu() {
 
   return (
     <div className={styles.menu}>
-      <Link
+      <RouterLink
         className={styles.menuLink}
-        to="/"
+        href="/"
         aria-label="Ir para a Home"
         title="Ir para a Home"
       >
         <HouseIcon />
-      </Link>
-      <a
+      </RouterLink>
+      <RouterLink
         className={styles.menuLink}
-        href="#"
+        href="/history/"
         aria-label="Ver Histórico"
         title="Ver Histórico"
       >
         <HistoryIcon />
-      </a>
-      <a
+      </RouterLink>
+      <RouterLink
         className={styles.menuLink}
-        href="#"
+        href="/settings/"
         aria-label="Configurações"
         title="Configurações"
       >
         <SettingsIcon />
-      </a>
+      </RouterLink>
       <a
         className={styles.menuLink}
         href="#"
