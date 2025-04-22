41f75afb0319c776ef3dc300616407c37f6afa12
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-04-21 22:09:55 -0300

    Fechando dialog de confirmação ao sair da página

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -47,6 +47,12 @@ export function History() {
     dispatch({ type: TaskActionTypes.RESET_STATE });
   }, [confirmClearHistory, dispatch]);
 
+  useEffect(() => {
+    return () => {
+      showMessage.dismiss();
+    };
+  }, []);
+
   function handleSortTasks({
     field,
   }: Omit<SortTasksOptions, "tasks" | "direction">) {
